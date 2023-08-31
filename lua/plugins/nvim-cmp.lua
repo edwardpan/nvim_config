46d6f38cbe36b64dcbeacebc12188ea8a7927a21
edwardpan <edwardp.61@gmail.com>
2023-08-30 22:14:57 -0400

    rename

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -8,6 +8,7 @@ local M = {
         'hrsh7th/cmp-emoji',
         'hrsh7th/cmp-path',
         'hrsh7th/cmp-nvim-lsp-signature-help',
+        'hrsh7th/cmp-cmdline',
         'saadparwaiz1/cmp_luasnip',
         'octaltree/cmp-look',
         'tzachar/cmp-tabnine',
